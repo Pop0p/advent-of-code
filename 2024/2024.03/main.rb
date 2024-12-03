63b9eea074c9e55e8a277fc9239084bc465103a7
Dylan Dromard <dylan.dromard@altazion.com>
2024-12-03 19:35:01 +0100

    Day 03 Ruby

---
+++ 2024/2024.03/main.rb
@@ -0,0 +1,14 @@
+﻿FILE_PATH = "./input.txt"
+
+file_content = File.read(FILE_PATH).gsub("\n", ' ')
+
+def calculate_sum(content, filter_regex = nil)
+  filtered_content = filter_regex ? content.gsub(filter_regex, '') : content
+  filtered_content.scan(/mul\((\d{1,3}),(\d{1,3})\)/).sum { |a, b| a.to_i * b.to_i }
+end
+
+sum_one = calculate_sum(file_content)
+sum_two = calculate_sum(file_content, /don't\(\)(.*?)(do\(\)|$)/)
+
+puts sum_one
+puts sum_two
