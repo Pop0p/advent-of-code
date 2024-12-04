7e1ff2aa9e1c210b93c64f46e4b772a7120c9cf8
Dylan Dromard <dylan.dromard@altazion.com>
2024-12-04 16:30:18 +0100

    wip

--- 2024/2024.04/main.js
+++ 2024/2024.04/main.js
@@ -1,8 +1,10 @@
 ﻿const fs = require("fs");
 let lines = fs.readFileSync("./input.txt", "utf8").trim();
-let total = 0;
 const grid = lines.split("\n").map((line) => line.replace("\r", "").split(""));
-const directions = [
+
+let part_one = 0;
+let part_two = 0;
+const directions_part_one = [
   { x: -1, y: 0 },
   { x: -1, y: 1 },
   { x: 0, y: 1 },
@@ -12,37 +14,66 @@ const directions = [
   { x: 0, y: -1 },
   { x: -1, y: -1 },
 ];
-const word = "XMAS";
 
 for (let y = 0; y < grid.length; y++) {
   const row = grid[y];
   for (let x = 0; x < row.length; x++) {
-    const char = row[x];
-    if (char != "X") continue;
-    scanForWord(x, y, 1);
+    if (row[x] == "X") {
+      for (let direction of directions_part_one) {
+        if (scan_part_one("XMAS", x, y, direction.x, direction.y, 1))
+          part_one += 1;
+      }
+    }
+    if (row[x] == "A") {
+      if (scan_part_two(x, y)) {
+        part_two += 1;
+      }
+    }
   }
 }
 
-function scanForWord(x, y, current_index) {
-  let found = true;
-  for (let direction of directions) {
-    if (x == 0 && direction.x == -1) continue;
-    if (x == grid[0].length - 1 && direction.x == 1) continue;
-    if (y == 0 && direction.y == -1) continue;
-    if (y == grid.length - 1 && direction.y == 1) continue;
-    const char = grid[y + direction.y][x + direction.x];
-    const searched_char = word[current_index];
-    if (char == searched_char) {
-      if (current_index == word.length - 1) {
-        return true;
-      }
-      const result = scanForWord(x + direction.x, y + direction.y, current_index + 1);
-      if (result)
-        return true
-    } else {
-      found = false;
-    }
-  }
-  if (!found) return false;
+function scan_part_one(word, from_x, from_y, to_x, to_y, current_word_index) {
+  if (isOutOfBounds(from_x, from_y, to_x, to_y, grid[0].length, grid.length))
+    return false;
+  if (grid[from_y + to_y][from_x + to_x] != word[current_word_index])
+    return false;
+  if (current_word_index == word.length - 1) return true;
+  return scan_part_one(
+    word,
+    from_x + to_x,
+    from_y + to_y,
+    to_x,
+    to_y,
+    current_word_index + 1
+  );
 }
-console.log(total);
+
+function scan_part_two(from_x, from_y) {
+  if (isOutOfBounds(from_x, from_y, 1, 1, grid[0].length, grid.length))
+    return false;
+  if (isOutOfBounds(from_x, from_y, -1, 1, grid[0].length, grid.length))
+    return false;
+  if (isOutOfBounds(from_x, from_y, -1, -1, grid[0].length, grid.length))
+    return false;
+  if (isOutOfBounds(from_x, from_y, 1, -1, grid[0].length, grid.length))
+    return false;
+
+  const NW = grid[from_y - 1][from_x - 1];
+  const NE = grid[from_y - 1][from_x + 1];
+  const SW = grid[from_y + 1][from_x - 1];
+  const SE = grid[from_y + 1][from_x + 1];
+
+  if (NW + SE != "MS" && NW + SE != "SM") return false;
+  if (NE + SW != "MS" && NE + SW != "SM") return false;
+
+  return true;
+}
+
+function isOutOfBounds(from_x, from_y, to_x, to_y, width, height) {
+  if (from_x == 0 && to_x == -1) return true;
+  if (from_y == 0 && to_y == -1) return true;
+  if (from_x == width - 1 && to_x == 1) return true;
+  if (from_y == height - 1 && to_y == 1) return true;
+}
+
+console.log(`part one : ${part_one}, part two : ${part_two}`);
