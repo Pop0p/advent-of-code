76e05013bc8c339929258ff3b81a8cab91de5074
Dylan Dromard <dylan.dromard@altazion.com>
2024-12-27 03:15:48 +0100

    Day 11 in C#

---
+++ 2024/2024.S11/Program.cs
@@ -0,0 +1,35 @@
+﻿var memory = File.ReadAllText("./input.txt").Split(" ").Select(ulong.Parse).GroupBy(n => n).ToDictionary(group => group.Key, group => (ulong)group.Count());
+for (var i = 0; i < 75; i++)
+{
+    var nextMemory = new Dictionary<ulong, ulong>();
+    
+    foreach (var (key, count) in memory.ToList())
+    {
+        foreach (var result in ApplyRules(key))
+        {
+            if (!nextMemory.TryAdd(result, count))
+                nextMemory[result] += count;
+        }
+    }
+
+    memory = nextMemory;
+}
+
+
+Console.WriteLine(memory.Values.Where(x => x > 0).Aggregate((a,b) => a+b));
+return;
+
+ulong[] ApplyRules(ulong number)
+{
+    var digits = (int)Math.Floor(Math.Log10(number) + 1);
+
+    if (number == 0)
+        return [1];
+
+    if (digits % 2 != 0)
+        return [number * 2024];
+
+    var numberOne = number.ToString()[..(digits / 2)];
+    var numberTwo = number.ToString()[(digits / 2)..];
+    return [uint.Parse(numberOne), uint.Parse(numberTwo)];
+}
