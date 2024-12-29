505a5d24be340f2b04faa6a5b17544dded32c134
Dylan <dromarddylan@gmail.com>
2024-12-29 20:12:07 +0100

    Day 12 and 13 in C# (#3)
    Day 12 in C#
    Day 13 in C#

---
+++ 2024/2024.12/Program.cs
@@ -0,0 +1,77 @@
+﻿var grid = File.ReadLines("./input.txt").Select(x => x.ToCharArray()).ToArray();
+
+var visited = new List<(int, int)>();
+var queue = new Queue<(int, int)>();
+(int, int)[] directions = [(-1, 0), (1, 0), (0, -1), (0, +1)];
+
+
+int totalPartOne = 0;
+int totalPartTwo = 0;
+
+for (int yy = 0; yy < grid.Length; yy++)
+{
+    for (int xx = 0; xx < grid.Length; xx++)
+    {
+        if (visited.Contains((yy, xx)))
+            continue;
+        
+        int currentGroupArea = 1;
+        int currentGroupPerimeterPartOne = 0;
+        int currentGroupPerimeterPartTwo = 4;
+        List<(int, int)> currentGroupContacts = [];
+
+        queue.Enqueue((yy, xx));
+        while (queue.Count > 0)
+        {
+            var (y, x) = queue.Dequeue();
+            var cell = grid[y][x];
+
+            foreach (var direction in directions)
+            {
+                var newX = x + direction.Item1;
+                var newY = y + direction.Item2;
+
+                if (newX == -1 || newY == -1 || newY == grid.Length || newX == grid.Length || grid[newY][newX] != cell)
+                {
+                    currentGroupPerimeterPartOne += 1;
+                    if (direction.Item2 != 0)
+                        currentGroupContacts.Add((newY + (y * 10), newX)); // Adding an offset so we don't have any overlap.   
+                }
+                else
+                {
+                    if (!visited.Contains((newY, newX)) && !queue.Contains((newY, newX)))
+                    {
+                        currentGroupArea += 1;
+                        queue.Enqueue((newY, newX));
+                    }
+                }
+            }
+
+            visited.Add((y, x));
+        }
+
+        if (currentGroupArea > 1)
+        {
+            var horizontalLines = currentGroupContacts
+                .GroupBy(p => p.Item1)
+                .SelectMany(g => g
+                    .OrderBy(p => p.Item2)
+                    .Aggregate(new List<List<(int, int)>>(), (acc, current) =>
+                    {
+                        if (acc.Count == 0 || current.Item2 != acc.Last().Last().Item2 + 1)
+                            acc.Add([current]);
+                        else
+                            acc.Last().Add(current); 
+                        return acc;
+                    }))
+                .ToList();
+            currentGroupPerimeterPartTwo = horizontalLines.Count * 2;
+        }
+
+        totalPartOne += currentGroupArea * currentGroupPerimeterPartOne;
+        totalPartTwo += currentGroupArea * currentGroupPerimeterPartTwo;
+    }
+}
+
+Console.WriteLine(totalPartOne);
+Console.WriteLine(totalPartTwo);
---
+++ 2024/2024.13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+var buttonRegex = new Regex(@"Button (A|B): X\+(\d+), Y\+(\d+)");
+var prizeRegex = new Regex(@"Prize: X=(\d+), Y=(\d+)");
+
+long minimalCostNoOffset = 0;
+long minimalCostWithOffset = 0;
+
+var lines = File.ReadAllLines("input.txt").Where(arg => !string.IsNullOrWhiteSpace(arg)).ToArray();
+for (int i = 0; i < lines.Length; i += 3)
+{
+    var buttonA = buttonRegex.Match(lines[i]);
+    var buttonB = buttonRegex.Match(lines[i + 1]);
+    var prize = prizeRegex.Match(lines[i + 2]);
+
+    // https://www.youtube.com/watch?v=jBsC34PxzoM
+    // https://www.youtube.com/watch?v=vXqlIOX2itM
+    if (buttonA.Success && buttonB.Success && prize.Success)
+    {
+        int Ax = int.Parse(buttonA.Groups[2].Value);
+        int Ay = int.Parse(buttonA.Groups[3].Value);
+
+        int Bx = int.Parse(buttonB.Groups[2].Value);
+        int By = int.Parse(buttonB.Groups[3].Value);
+
+        long Cx = long.Parse(prize.Groups[1].Value);
+        long Cy = long.Parse(prize.Groups[2].Value);
+
+        foreach (bool offset in new[] { false, true })
+        {
+            long offsetValue = offset ? 10000000000000 : 0;
+
+            long adjustedCx = Cx + offsetValue;
+            long adjustedCy = Cy + offsetValue;
+
+            long A = (adjustedCx * By - adjustedCy * Bx) / (Ax * By - Ay * Bx);
+            long B = (Ax * adjustedCy - Ay * adjustedCx) / (Ax * By - Ay * Bx);
+
+            if (adjustedCx == Ax * A + Bx * B && adjustedCy == Ay * A + By * B)
+            {
+                if (!offset)
+                    minimalCostNoOffset += 3 * A + B;
+                else
+                    minimalCostWithOffset += 3 * A + B;
+            }
+        }
+    }
+}
+Console.WriteLine(minimalCostNoOffset);
+Console.WriteLine(minimalCostWithOffset);
