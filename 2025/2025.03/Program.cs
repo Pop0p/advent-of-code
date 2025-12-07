f5db9cbd880c691f9d14a6d139d3e8031a0c1894
Dylan Dromard <dylan.dromard@altazion.com>
2025-12-07 14:58:06 +0100

    2025: Day 03 in C#

---
+++ 2025/2025.03/Program.cs
@@ -0,0 +1,31 @@
+﻿using helpers;
+
+var test = "987654321111111\n811111111111119\n234234234234278\n818181911112111";
+var data = await Helper.GetData (3);
+
+
+Console.WriteLine (GetJoltage (2));
+Console.WriteLine (GetJoltage (12));
+return;
+
+UInt64 GetJoltage (Int32 needed)
+{
+  UInt64 p = 0;
+  foreach (var line in Helper.ReadLines (data))
+  {
+    var batteries = line.ToCharArray ().Select (c => Int32.Parse (c.ToString ())).ToArray ();
+    var current   = String.Empty;
+    var skip      = 0;
+    while (current.Length < needed)
+    {
+      var remaining         = batteries.Skip (skip).ToArray ();
+      var searchLength       = remaining.Length - (needed - current.Length) + 1;
+      var searchable        = remaining [..searchLength].ToArray ();
+      var highestValueIndex     = searchable.IndexOf (searchable.Max ());
+      current += searchable [highestValueIndex].ToString ();
+      skip    += highestValueIndex + 1;
+    }
+    p += UInt64.Parse (current);
+  }
+  return p;
+}
