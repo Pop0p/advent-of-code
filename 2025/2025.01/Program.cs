9ecc0d1a4630b8cb691bd8d45480a7457f8910cb
Dylan Dromard <dylan.dromard@altazion.com>
2025-12-07 11:54:07 +0100

    2025 : Day 01 and 02 in C#

---
+++ 2025/2025.01/Program.cs
@@ -0,0 +1,49 @@
+﻿using helpers;
+
+var test = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
+
+var data = await Helper.GetData(1);
+
+var value  = 50;
+var p1     = 0;
+var p2     = 0;
+foreach (var line in Helper.ReadLines(data))
+{
+  var direction = line[0];
+  var distance  = Int32.Parse(line[1..]);
+
+  switch (direction)
+  {
+    case 'L' :
+    {
+      for (var i = distance; i > 0; i --)
+      {
+        value -= 1;
+        if (value == 0)
+          p2 += 1;
+        else if (value < 0)
+          value =  99;
+      }
+      break;
+    }
+
+    case 'R' :
+    {
+      value += distance;
+      if (value > 99)
+      {
+        p2    += (int)Math.Floor(value / 100f);
+        value %= 100;
+      }
+      break;
+    }
+  }
+  if (value == 0)
+    p1++;
+    
+
+}
+
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.WriteLine(value);
---
+++ 2025/2025.02/Program.cs
@@ -0,0 +1,52 @@
+﻿using helpers;
+
+var test  = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+var data  = await Helper.GetData (2);
+var parts = Helper.SplitLine (',', data);
+
+
+UInt64 p1 = 0;
+UInt64 p2 = 0;
+foreach (var part in parts)
+{
+  var ranges = part.Split ('-');
+
+  var start   = UInt64.Parse (ranges [0]);
+  var end     = UInt64.Parse (ranges [1]);
+  var current = start;
+
+  while (current <= end)
+  {
+    var str        = current.ToString ();
+    var differents = str.Distinct ().Count ();
+
+
+    if (str.Length % 2 == 0)
+    {
+      var half       = str.Length / 2;
+      var firstPart  = str [..half];
+      var secondPart = str [half..];
+
+      if (firstPart == secondPart)
+      {
+        p1 += current;
+        p2 += current;
+        current ++;
+        continue;
+      }
+    }
+    if (str.Length % differents == 0 && differents != str.Length)
+    {
+      var chunks = new List <String> ();
+
+      for (var i = 0; i < str.Length; i += differents)
+        chunks.Add (str.Substring (i, differents));
+
+      if (chunks.All (chunk => chunk == chunks [0]))
+        p2 += current;
+    }
+    current ++;
+  }
+}
+Console.WriteLine (p1);
+Console.WriteLine (p2);
